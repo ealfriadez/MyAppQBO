ed85b58f787255e8bcd9959282db5d5638ecf385
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-06-24 15:04:12 -0500

    fallas en codigo

--- app/src/main/java/pe/edu/unfv/myappqbo/RegistroActivity.kt
+++ app/src/main/java/pe/edu/unfv/myappqbo/RegistroActivity.kt
@@ -2,26 +2,81 @@ package pe.edu.unfv.myappqbo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.RadioButton
+import com.google.android.material.snackbar.Snackbar
 
 class RegistroActivity : AppCompatActivity() {
 
-    lateinit var editNombre: EditText
-    //lateinit var editPasswor: EditText
+    lateinit var edit_Nombre: EditText
+    lateinit var rb_Genero: RadioButton
+    lateinit var chk_Deporte: CheckBox
+    lateinit var chk_Dibujo: CheckBox
+    lateinit var chk_Otros: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
     }
 
-    fun validarNombre(): Boolean{
-        var respuesta = true
-        editNombre = findViewById(R.id.etNombre)
+    fun obtenerGeneroSeleccionado(): String{
+        var genero = ""
+        rb_Genero = findViewById(R.id.rgGenero)
 
-        if (editNombre.text.toString().trim().isEmpty()){
-            editNombre.isFocusableInTouchMode = true
-            editNombre.requestFocus()
+        when(rb_Genero.id)
+    }
+
+    fun validarFormulario(vista: View): Boolean{
+        var respuesta = false
+        if(!validarNombre()){
+            enviarMensajeError(vista, "Es obligatorio su nombre")
+        }else if (!validarGenero()){
+            enviarMensajeError(vista, "Es obligatorio su genero")
+        }else if (!validarPreferencias()) {
+            enviarMensajeError(vista, "Es obligatorio su genero")
+        }else{
+            respuesta = true
+        }
+        return respuesta
+    }
+
+    fun enviarMensajeError(vista: View, mensajeError: String){
+        Snackbar.make(vista, mensajeError, Snackbar.LENGTH_LONG).show()
+    }
+
+    fun validarPreferencias(): Boolean{
+        var respuesta = false
+        chk_Deporte = findViewById(R.id.chkDeporte)
+        chk_Dibujo = findViewById(R.id.chkDibujo)
+        chk_Otros = findViewById(R.id.chkOtros)
+
+        if (chk_Deporte.isChecked || chk_Dibujo.isChecked || chk_Otros.isChecked){
+            respuesta = true
+        }
+        return respuesta
+    }
+
+    fun validarGenero(): Boolean{
+        var respuesta = true
+        rb_Genero = findViewById(R.id.rgGenero)
+
+        if (rb_Genero.isChecked){
             respuesta = false
         }
+        return respuesta
+    }
+
+    fun validarNombre(): Boolean{
+        var respuesta = true
+        edit_Nombre = findViewById(R.id.etNombre)
+
+        if (edit_Nombre.text.toString().trim().isEmpty()){
+            edit_Nombre.isFocusableInTouchMode = true
+            edit_Nombre.requestFocus()
+            respuesta = false
+        }
+        return respuesta
     }
 }
